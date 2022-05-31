1989596e7b8fa3fb8343f98adfffbdf289efabbc
Rene <ddcrene@gmail.com>
2022-05-31 11:56:58 -0400

    TDD 2 F

--- src/TDD.test.js
+++ src/TDD.test.js
@@ -7,6 +7,11 @@ describe("TDD", () => {
       let monto = devolverMonto(19);
       expect(monto).toEqual(19);
     });
+
+    it("Ingresar el monto y devolver resta con la moneda de 5", () => {
+        let cambio = devolverMonto(19);
+        expect(cambio).toEqual(14);
+    });
 });  
 
 function devolverMonto(monto){
